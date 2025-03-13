b5c5e57471f8ecc0aa91baad2f99314c1fa9c9ef
Ayush Rathore <131510947+ayushrathore1@users.noreply.github.com>
2025-03-13 21:22:20 +0530

    experimental change 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -125,7 +125,7 @@ export default {
 			},
 			backgroundImage: {
 				// 'forest': "url('https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80')",
-				'forest-dark': "url('https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80')"
+				// 'forest-dark': "url('https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80')"
 			}
 		}
 	},
