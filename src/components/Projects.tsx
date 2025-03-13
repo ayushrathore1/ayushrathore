a8bda39a61d7683724b85fab425a6a4ee345a058
Ayush Rathore <131510947+ayushrathore1@users.noreply.github.com>
2025-03-14 00:22:04 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -36,6 +36,7 @@ const projects: Project[] = [
     image:
       "https://images.unsplash.com/photo-1618401471353-b98afee0b2eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80",
     technologies: ["Vibe Coding", "Some Debugging"],
+      liveLink: "https://portfolio-ayushrathore.lovable.app/",
     githubLink: "https://github.com/ayushrathore1/eco-journey-showcase",
   },
 ];
