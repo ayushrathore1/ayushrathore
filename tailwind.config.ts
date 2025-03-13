bd952eaca4a05d6da22034fddd95b7e9b1f85168
Ayush Rathore <131510947+ayushrathore1@users.noreply.github.com>
2025-03-13 21:20:54 +0530

    experimental change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -124,7 +124,7 @@ export default {
 				serif: ['Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif']
 			},
 			backgroundImage: {
-				'forest': "url('https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80')",
+				// 'forest': "url('https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80')",
 				'forest-dark': "url('https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80')"
 			}
 		}
